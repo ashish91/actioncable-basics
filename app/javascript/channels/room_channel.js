a13d8ef970647aed562975f361fc7914fe545801
Ashish Gaur <ashishgaurp91@gmail.com>
2021-05-17 00:47:25 +0530

    Initial commit

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,22 @@
+import consumer from "./consumer"
+
+document.addEventListener('turbolinks:load', () => {
+  const element = document.getElementById('room-id');
+  const roomId = element.getAttribute('data-room-id');
+
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
+    connected() {
+      console.log("Connected to RoomChannel" + roomId);
+      // Called when the subscription is ready for use on the server
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data);
+    }
+  });
+});
